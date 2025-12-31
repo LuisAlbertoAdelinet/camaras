2e6ee44f3c01df4da5ec26d90c3ca90b75b1b7f0
Lisandro <you@example.com>
2025-12-31 20:07:33 -0300

    Fix: Final alignment of dependencies and stabilization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,10 +59,10 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("io.coil-kt:coil-compose:2.6.0")
+    
+    // JSON & Network
     implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.24")
-
-    // Retrofit & Network
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
